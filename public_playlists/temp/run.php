e6d45bb1df8bfdcbccf4f2a41e9d77dab7f1f311
Scott R McGann <s.r.mcgann@hotmail.com>
2023-05-12 16:21:40 -0700

    Update run.php

--- public_playlists/temp/run.php
+++ public_playlists/temp/run.php
@@ -13,6 +13,6 @@
   echo shell_exec("unzip -P PW temp.zip & 2>&1") . "<br>";
   echo shell_exec("chmod 777 . -R 2>&1") . "<br>";
   echo shell_exec("mv roadtrip-tracks/* .") . "<br>";
-  echo shell_exec("ls") . "<br>";
+  echo str_replace("\n", "<br>", shell_exec("ls")) . "<br>";
   echo "<br>finished expaning archive.<br>";
 ?>
