562df6ead40c0e5a5fe6f2507a2219983fddd2fd
Scott R McGann <s.r.mcgann@hotmail.com>
2023-05-12 15:21:09 -0700

    Update run.php

--- public_playlists/temp/run.php
+++ public_playlists/temp/run.php
@@ -7,7 +7,10 @@
   echo shell_exec("wget https://www.dropbox.com/s/p588lmnqxnyfp3f/temp.zip.003?raw=1 -O temp.zip.003");
   echo shell_exec("wget https://www.dropbox.com/s/3ys4t6jckwep3fo/temp.zip.004?raw=1 -O temp.zip.004");
   echo shell_exec("wget https://www.dropbox.com/s/wu0t9niyz8d178q/temp.zip.005?raw=1 -O temp.zip.005");
-  echo "done downloading files.";
-  //echo shell_exec("unzip -P PW temp.zip") . "\n";
-  //echo shell_exec("chmod 777 . -R") . "\n";
+  echo "done downloading files.\n";
+  echo shell_exec("cat temp.zip* > temp.zip");
+  echo "finished concatenating archive parts.\n";
+  echo shell_exec("screen -d -m unzip -P PW temp.zip") . "\n";
+  echo shell_exec("chmod 777 . -R") . "\n";
+  echo "finished expaning archive.\n";
 ?>
